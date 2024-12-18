395159f69a68f9ce077088b39d74784e153ee093
Akil <akil.siddique@postex.pk>
2024-12-19 01:08:47 +0500

    add news details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,19 @@ import { TableComponent } from './table/table.component';
 import { NewCreateComponent } from './new-create/new-create.component';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutComponent } from './layout/layout.component'; 
+import { NewsDetailsComponent } from './components/news-details/news-details.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent, 
+    component: LayoutComponent,
     children: [
-      { path: '', component: HomeComponent },  
+      { path: '', component: HomeComponent },
       { path: 'news-logs', component: TableComponent },
-      { path: 'create-news', component: NewCreateComponent },  
+      { path: 'create-news', component: NewCreateComponent },
+      {
+        path: 'news-details/:id', component: NewsDetailsComponent
+      },
       { path: 'news', loadChildren: () => import('./news/news.module').then(m => m.NewsModule) },
     ]
   },
